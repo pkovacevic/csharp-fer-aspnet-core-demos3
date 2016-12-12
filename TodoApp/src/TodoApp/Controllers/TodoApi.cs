293a6695564e1ab25e6de9c808545259eb39ef91
Petar Kovacevic <petar.kovacevic@axilis.com>
2016-12-12 22:46:52 +0100

    todo api controller example

---
+++ TodoApp/src/TodoApp/Controllers/TodoApi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using TodoApp.Interfaces;
+
+namespace TodoApp.Controllers
+{
+    public class TodoApi : Controller
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoApi(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Get()
+        {
+            var result = _repository.GetLastTodos(10);
+
+            return new ObjectResult(result);
+        }
+
+    }
+}
